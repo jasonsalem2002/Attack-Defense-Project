95fa4e581e9ed59cbcd13ddb622e69cea05e884a
Hussein Mallah <mallahhuos3009@gmail.com>
2023-09-27 12:56:29 -0400

    Change IP
    Same as Jason's server but slight change to ip for testing

---
+++ Huss/ServerTest_Hussein.py
@@ -0,0 +1,24 @@
+import socket
+# Jason
+
+# Server IP and port
+server_ip = '127.0.0.1'  # Listen on all available network interfaces
+server_port = 12345  # Choose an available port
+
+# Create a UDP socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Bind the socket to the server address
+server_socket.bind((server_ip, server_port))
+
+print(f"Listening for UDP packets on {server_ip}:{server_port}")
+
+while True:
+    # Receive data from the client
+    data, client_address = server_socket.recvfrom(1024)
+
+    # Decode and print the received data
+    message = data.decode()
+    print(f"Received from {client_address}: {message}")
+
+# Jason end
